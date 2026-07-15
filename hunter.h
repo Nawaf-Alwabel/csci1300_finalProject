4155ffa4bb9af0653eaced9ba993c9a0c8e5a0f8
Nawaf-Alwabel <nawafalwabel01@gmail.com>
2026-07-14 23:44:36 -0600

    hunter.h

--- hunter.h
+++ hunter.h
@@ -2,21 +2,15 @@
 #define HUNTER_H
 
 #include "Character.h"
+#include "Item.h"
 
 class Hunter : public Character
 {
-private:
-    int hunting_bonus;
-
 public:
     Hunter();
-    Hunter(std::string character_name, int starting_money,
-           int starting_reputation, int bonus);
+    Hunter(string characterName, string characterLocation);
 
-    int getHuntingBonus() const;
-
-    void hunt();
-    void showRole() const override;
+    Item hunt();
 };
 
 #endif
