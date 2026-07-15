d32b390385baaeb58fe456d94092109c0d6ac767
Nawaf-Alwabel <nawafalwabel01@gmail.com>
2026-07-14 23:45:22 -0600

    Player.h

---
+++ Player.h
@@ -0,0 +1,51 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Item.h"
+#include <string>
+using namespace std;
+
+class Player
+{
+private:
+    string name;
+    string location;
+
+    int day;
+    int energy;
+    int money;
+    int influence;
+
+    Item inventory[10];
+    int inventorySize;
+
+public:
+    Player();
+
+    void setName(string playerName);
+
+    string getName();
+    string getLocation();
+
+    int getDay();
+    int getEnergy();
+    int getMoney();
+    int getInfluence();
+
+    void moveTo(string newLocation);
+
+    void useEnergy(int amount);
+    void restoreEnergy();
+
+    void addMoney(int amount);
+    bool spendMoney(int amount);
+
+    void addInfluence(int amount);
+    void nextDay();
+
+    void addItem(Item item);
+    void displayInventory();
+    void sellInventory();
+};
+
+#endif
