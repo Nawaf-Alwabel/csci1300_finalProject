11153944f6a945560554c1d9d727541ecc4c3984
Nawaf-Alwabel <nawafalwabel01@gmail.com>
2026-07-14 23:46:04 -0600

    game.h

--- game.h
+++ game.h
@@ -1,27 +1,57 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "Player.h"
+#include "Character.h"
 #include "Fisher.h"
 #include "Hunter.h"
+
+#include <string>
+using namespace std;
 
 class Game
 {
 private:
-    bool running;
-    int day;
+    Player player;
+
+    Character candidate;
+    Character alchemist;
+    Character gambler;
 
     Fisher fisher;
     Hunter hunter;
 
-    void displayMainMenu() const;
-    void displayStatus() const;
-    void performDailyAction();
-    void advanceDay();
+    string dialogue[5];
+
+    bool gameOver;
+    bool jailEnding;
 
 public:
     Game();
 
-    void run();
+    void loadDialogue();
+    void startGame();
+
+    void displayDashboard();
+    void displayMap();
+    void displayMenu();
+
+    void processChoice(int choice);
+    void movePlayer();
+    void locationAction();
+
+    void villageAction();
+    void fishingAction();
+    void huntingAction();
+    void alchemistAction();
+    void gamblingAction();
+
+    void endDay();
+
+    bool checkWin();
+    bool checkLoss();
+
+    void displayEnding();
 };
 
 #endif
